6b1d83443776569d153338be7c2ff4d3e89bb23f
DE\KMT2ABT <martin.kastner@de.bosch.com>
2025-10-07 15:35:14 +0200

    klasse für das speichern des word pfades erstellt

--- Main_Phobetor.py
+++ Main_Phobetor.py
@@ -2,6 +2,55 @@ import pygame
 import sys
 import config
 import time
+import datetime
+
+
+class KeyWordTree:
+    def __init__(self):
+        self.data = {}
+
+    def add(self, *path):
+        """Flexibles Hinzufügen: add('a', 'ab', 'aba') oder add(['a', 'ab', 'aba'])"""
+        if len(path) == 1 and isinstance(path[0], (list, tuple)):
+            path = path[0]
+
+        current = self.data
+        for key in path:
+            if key not in current:
+                current[key] = {}
+            current = current[key]
+
+    def search(self, *path):
+        """Flexibles Suchen"""
+        if len(path) == 1 and isinstance(path[0], (list, tuple)):
+            path = path[0]
+
+        current = self.data
+        for key in path:
+            if key not in current:
+                return False, []
+            current = current[key]
+
+        return True, sorted(current.keys())
+
+    def exists(self, *path):
+        """Prüft nur Existenz"""
+        exists, _ = self.search(*path)
+        return exists
+
+
+# # Sehr einfache Verwendung:
+# store = FlexibleKeywordStore()
+
+# # Verschiedene Wege zum Hinzufügen
+# store.add('a', 'aa', 'aaa')
+# store.add(['a', 'ab', 'aba'])
+# store.add(['a', 'ab', 'abb'])
+
+# # Verschiedene Wege zum Suchen
+# print(store.search('a'))           # (True, ['aa', 'ab'])
+# print(store.search(['a', 'ab']))   # (True, ['aba', 'abb'])
+# print(store.exists('a', 'ab'))     # True
 
 
 class Console:
